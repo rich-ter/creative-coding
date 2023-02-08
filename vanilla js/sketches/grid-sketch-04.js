e31d9c9b740d18bebe11717e2229c03b1a26bac2
rich-ter <me@philipposrichter.com>
2023-02-08 23:02:07 +0200

    finishing grid sketch 4

--- vanilla js/sketches/grid-sketch-04.js
+++ vanilla js/sketches/grid-sketch-04.js
@@ -8,14 +8,25 @@ const settings = {
   animate: true
 };
 
+const params = {
+  cols:10,
+  rows:10,
+  scaleMin:1,
+  scaleMax:30,
+  freq: 0.001,
+  amp:0.2,
+  frame: 0,
+  animate: true,
+  lineCap: 'butt',
+}
+
 const sketch = () => {
   return ({ context, width, height, frame}) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
-
+    const cols = params.cols;
+    const rows = params.rows;
     const numCells = cols * rows;
 
     const gridWidth = width * 0.8;
@@ -36,9 +47,11 @@ const sketch = () => {
       const w = cellWidth * 0.8;
       const h = cellHeight * 0.8;
 
-      const n = random.noise2D(x+frame*10,y, 0.001);
-      const angle = n * Math.PI * 0.2;
-      const scale = math.mapRange(n, -1,1,1,30);
+      const f = params.animate ? frame : params.frame
+
+      const n = random.noise3D(x,y, frame*10, params.freq);
+      const angle = n * Math.PI * params.amp;
+      const scale = math.mapRange(n, -1,1,params.scaleMin,params.scaleMax);
 
       context.save();
       context.translate(x,y);
@@ -47,6 +60,7 @@ const sketch = () => {
       context.rotate(angle);
 
       context.lineWidth = scale;
+      context.lineCap = params.lineCap
 
       context.beginPath();
       context.moveTo(w*-0.5,0);
@@ -59,8 +73,25 @@ const sketch = () => {
   };
 };
 
+//EXTERNAL API LIBRARY FOR CONTROLLER OF GRID.
 const createPane = () => {
   const pane = new Tweakpane.Pane();
+  let folder;
+
+
+  folder = pane.addFolder({title: 'Grid'})
+  folder.addInput(params, 'lineCap', {options:{butt: 'butt', round: 'round', square:'square'}})
+  folder.addInput(params, 'cols', {min:2,max:50,step:1})
+  folder.addInput(params, 'rows', {min:2,max:50,step:1})
+  folder.addInput(params, 'scaleMin', {min:1,max:100})
+  folder.addInput(params, 'scaleMax', {min:1,max:100})
+
+  folder = pane.addFolder({title: 'Noise'})
+  folder.addInput(params, 'freq', {min: -0.01, max:0.01})
+  folder.addInput(params, 'amp', {min: 0, max:1})
+  folder.addInput(params, 'animate')
+  folder.addInput(params, 'frame', {min: 0, max:999})
+
 };
 
 createPane()
